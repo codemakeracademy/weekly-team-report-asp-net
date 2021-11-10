afd73c4a9a7f74643f04ed267431cdd4d60e85fd
Anton Semenkov <k3gbdia1@gmail.com>
2021-11-10 23:52:42 +0300

    feat: WeeklyReport implementation

--- CM.WeeklyTeamReport/src/CM.WeeklyTeamReport.Domain/WeeklyReport.cs
+++ CM.WeeklyTeamReport/src/CM.WeeklyTeamReport.Domain/WeeklyReport.cs
@@ -8,5 +8,15 @@ namespace CM.WeeklyTeamReport.Domain
 {
     public class WeeklyReport
     {
+        public DayOfWeek DateStart { get; private set; }
+        public double Duration { get; set; }
+        public string Task { get; set; }
+
+        public WeeklyReport(DayOfWeek dateStart, double duration, string task)
+        {
+            DateStart = dateStart;
+            Duration = duration;
+            Task = task;
+        }
     }
 }
