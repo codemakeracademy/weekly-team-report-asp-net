9a7827c2e63b373b5ef83afce1038a9d1d2baa91
Ilya <goneron@mail.ru>
2021-11-10 22:28:55 +0700

    feat: create dontet solution

---
+++ CM.WeeklyTeamReport/src/CM.WeeklyTeamReport.Domain/Company.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CM.WeeklyTeamReport.Domain
+{
+    public class Company
+    {
+        public Company(string CompanyName, List<TeamMember> TeamMembers)
+        {
+            this.CompanyName = CompanyName; this.TeamMembers = TeamMembers;
+        }
+        public string CompanyName { get; set; }
+        public List<TeamMember> TeamMembers { get; set; }
+    }
+}
---
+++ CM.WeeklyTeamReport/src/CM.WeeklyTeamReport.Domain/TeamMember.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CM.WeeklyTeamReport.Domain
+{
+    public class TeamMember
+    {
+        public TeamMember(string FirstName, string LastName, TypeOfTeam typeOfTeam)
+        {
+            this.FirstName = FirstName; this.LastName = LastName; this.typeOfTeam = typeOfTeam;
+            teamMemberReports = new List<WeeklyReport>();
+        }
+        
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public TypeOfTeam typeOfTeam{ get; private set; }
+        public List<WeeklyReport> teamMemberReports { get; private set; }
+        public void CreateMemberReport(WeeklyReport report)
+        {
+            teamMemberReports.Add(report);
+        }
+        public void ChangeInformation(string? FirstName, string? LastName)
+        {
+            if (FirstName != null)
+                this.FirstName = FirstName;
+            if (LastName != null)
+                this.LastName = LastName;
+        }
+        public void DeleteReport(int idOfReport)
+        {
+            teamMemberReports.RemoveAt(idOfReport);
+        }
+    }
+    public enum TypeOfTeam
+    {
+        Immediate, Extended
+    }
+}
