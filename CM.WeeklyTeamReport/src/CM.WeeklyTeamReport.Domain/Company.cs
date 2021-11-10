b1e606e3d5eff450053f15e448c510d2a9621008
Anton Semenkov <k3gbdia1@gmail.com>
2021-11-10 23:34:55 +0300

    feat: company implementation

--- CM.WeeklyTeamReport/src/CM.WeeklyTeamReport.Domain/Company.cs
+++ CM.WeeklyTeamReport/src/CM.WeeklyTeamReport.Domain/Company.cs
@@ -4,5 +4,27 @@ namespace CM.WeeklyTeamReport.Domain
 {
     public class Company
     {
+        public string CompanyName { get; set; }
+        public string Country { get; set; }
+        public string City { get; }
+        private string _president;
+        public string President { 
+            get 
+            {
+                return _president;
+            } 
+        }
+
+        public Company(string companyName, string country, string city, string president)
+        {
+            CompanyName = companyName;
+            Country = country;
+            City = city;
+            _president = president;
+        }
+        public void NewPresident(string newPresident)
+        {
+            _president = newPresident;
+        }
     }
 }
