129be42e5f35867c6c9944df3762730004600d23
artj15 <artj15@gmail.com>
2021-11-14 11:27:44 +0300

    Classes created.

---
+++ CM.WeeklyTeamReport/test/CM.WeeklyTeamReport.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,32 @@
+using System;
+using Xunit;
+
+namespace CM.WeeklyTeamReport.Domain.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void ShouldCompanyCreated()
+        {
+            Company company = new Company();
+
+
+        }
+
+        [Fact]
+        public void ShouldTeamMemberCreated()
+        {
+            TeamMember teammember = new TeamMember();
+
+        }
+        
+        [Fact]
+
+        public void SouldWeeklyReportCreated()
+        {
+            WeeklyReport weeklyreport = new WeeklyReport();
+
+        }
+    }
+
+}
